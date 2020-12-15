755481f064707b3c66f65b6387e0752cbfa283bf
Salla Ojala <salla.ojalaa@gmail.com>
2020-12-15 08:05:24 +0200

    Day 15 number game

---
+++ 2020/day15.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+fn play_game(ref numbers:&Vec<usize>, turns:usize) -> usize
+{
+	let mut game:HashMap<usize, usize> = HashMap::new();
+	let mut last_spoken = numbers[0];
+	for i in 1..numbers.len()
+	{
+		game.insert(last_spoken, i - 1);
+		last_spoken = numbers[i];
+	}
+	
+	for i in numbers.len()..turns
+	{
+		let mut next_spoken;
+		match game.get(&last_spoken)
+		{
+			Some(value) => next_spoken = i - 1 - value,
+			None => next_spoken = 0
+		}
+		game.insert(last_spoken, i - 1);
+		last_spoken = next_spoken;
+	}
+	return last_spoken;
+}
+
+fn main() 
+{
+	let contents = "0,8,15,2,12,1,4";
+	let numbers:Vec<usize> = contents.split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+
+	println!("First star: {}", play_game(&numbers, 2020));
+	println!("Second star: {}", play_game(&numbers, 30000000));
+}
